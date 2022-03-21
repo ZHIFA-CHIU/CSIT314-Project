9b7d681b28d5e819d6eed2af3e2db7a6382db346
Pedro Oste <pedstarpedstar@yahoo.com.au>
2022-03-21 17:17:45 +1100

    Spring Intialisation, Customer Implementation

---
+++ src/test/java/com/csit314/roadSideAssistance/RoadSideAssistanceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.csit314.roadSideAssistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class RoadSideAssistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
