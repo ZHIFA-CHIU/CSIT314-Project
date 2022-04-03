93b1393f5ab20af3d9c46c1942f6e571dda2ef1c
Rynder1 <liam.ellis37@gmail.com>
2022-04-04 00:11:59 +1000

    Created a basic multipage react app
    Created a multi page basic app off a tutorial, different pages show their own content and have a shared nav bar for between them
    
    Followed: https://www.golangprograms.com/how-to-create-simple-react-router-to-navigate-multiple-pages.html
    
    while changing to V6 instructions based off: https://stackoverflow.com/questions/69854011/matched-leaf-route-at-location-does-not-have-an-element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
-
-export default class App extends Component {
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+ 
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+ 
+class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>Roadside Assitant Service</h1>
-      </div>
-    )
+    return (      
+       <BrowserRouter>
+        <div>
+          <Navigation />
+            <Routes>
+              <Route path="/" element={<Home/>} exact />
+              <Route path="/about" element={<About/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+
+            <Route component={Error}/>
+           </Routes>
+        </div> 
+      </BrowserRouter>
+    );
   }
 }
-
+ 
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const About = () => {
+    return (
+       <div>
+Dsjadsajdksa
+dsajdosajdkas
+sa
+       </div>
+    );
+}
+ 
+export default About;
