e9bf54471488f954e5db3c9fab30ec12a8dee408
Jack-Is-2048 <101245336+Jack-Is-2048@users.noreply.github.com>
2022-04-30 22:38:30 +1000

    Initial Technician Backend Creation

---
+++ src/main/java/com/csit314/roadSideAssistance/Technician/TechnicianController.java
@@ -0,0 +1,52 @@
+package com.csit314.roadSideAssistance.Technician;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Technician Controller
+ * Controlling:
+ *      Get
+ *      Post
+ *      Delete
+ *      Put
+ *
+ * @author      Jack_Is_2048
+ * @version     1.1
+ * @since       1.1
+ */
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping(path = "api/v1/technician")
+public class TechnicianController {
+
+    private final TechnicianService technicianService;
+
+    @Autowired
+    public TechnicianController(TechnicianService technicianService) {
+        this.technicianService = technicianService;
+    }
+
+    @GetMapping
+    public List<Technician> getTechnicians() {
+        return technicianService.getTechnician();
+    }
+
+    @PostMapping
+    public void registerTechnician(@RequestBody Technician technician) throws TechnicianException {
+        technicianService.registerTechnician(technician);
+    }
+
+    @DeleteMapping(path = "{technicianId}")
+    public void deleteTechnician(@PathVariable("technicianId") UUID technicianId) throws TechnicianException {
+        technicianService.deleteTechnician(technicianId);
+    }
+
+    @PutMapping
+    public void updateTechnician(@RequestBody Technician technician) throws TechnicianException {
+        technicianService.updateTechnician(technician);
+    }
+}
---
+++ src/test/java/com/csit314/roadSideAssistance/technicianTests/TechnicianTest.java
@@ -0,0 +1,60 @@
+package com.csit314.roadSideAssistance.technicianTests;
+
+import com.csit314.roadSideAssistance.Technician.Technician;
+import com.csit314.roadSideAssistance.Technician.TechnicianException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Technician tests
+ *      testCreateTechnician
+ *      testValidUser
+ *      addToAvgRating
+ *
+ * @author      Jack_Is_2048
+ * @version     1.1
+ * @since       1.1
+ */
+@SpringBootTest(classes = TechnicianTest.class)
+class TechnicianTest {
+
+    @Test
+    @DisplayName("Simple Test of Technician Creation")
+    void testCreateTechnician() throws TechnicianException {
+        Technician technician = new Technician("Jay", "Smith", "jaysmith@mail.com",
+                LocalDate.of(1990, Month.FEBRUARY, 12), "01234567890", "password");
+
+        assertEquals(technician.getFirstName(), "Jay", "Checking name");
+        assertEquals(technician.getEmail(), "jaysmith@mail.com", "Checking email");
+    }
+
+    @Test
+    @DisplayName("Simple Test of Valid User Constraint")
+    void testValidUser() throws TechnicianException {
+        TechnicianException thrown = Assertions.assertThrows(TechnicianException.class, () -> {
+            Technician technician = new Technician("Jay", "", "jaysmith@mail.com",
+                    LocalDate.of(1990, Month.FEBRUARY, 12), "01234567890", "password");
+        });
+    }
+
+    @Test
+    @DisplayName("Testing Technician adding to Avg Rating")
+    void addToAvgRating() throws TechnicianException {
+        Technician technician = new Technician("Jay", "Smith", "jaysmith@mail.com",
+                LocalDate.of(1990, Month.FEBRUARY, 12), "01234567890", "password");
+
+        technician.setAvgRating(3.5);
+
+        // Add a rating of 4
+        technician.addToAvgRating(4);
+
+        assertEquals(technician.getAvgRating(), 3.75, "Checking Avg Rating");
+    }
+}
