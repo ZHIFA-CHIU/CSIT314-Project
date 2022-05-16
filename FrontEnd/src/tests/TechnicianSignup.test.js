a8dd59fc37e874ca8c5dff94af9810f60d67d2a5
Rynder1 <liam.ellis37@gmail.com>
2022-05-17 09:46:25 +1000

    fixed tests
    added test

---
+++ FrontEnd/src/tests/TechnicianSignup.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import TechnicianSignup from '../pages/TechnicianSignup';
+
+test('Check all interaction labels', async () => {
+
+    <Router>
+        render(<TechnicianSignup />);
+
+        expect(screen.getByLabelText('Name')).toBeInTheDocument();
+        expect(screen.getByLabelText('Email')).toBeInTheDocument();
+        expect(screen.getByLabelText('Address')).toBeInTheDocument();
+        expect(screen.getByLabelText('Password')).toBeInTheDocument();
+        expect(screen.getByLabelText('Mobile number')).toBeInTheDocument();
+        expect(screen.getByLabelText('Date of Birth')).toBeInTheDocument();
+        expect(screen.getByLabelText('Police Check')).toBeInTheDocument();
+        expect(screen.getByLabelText('Qualification Certificate')).toBeInTheDocument();
+        expect(screen.getByLabelText('Account Number')).toBeInTheDocument();
+        expect(screen.getByLabelText('BSB')).toBeInTheDocument();
+    </Router>
+
+});
+
+/*test("", () =>{
+seperate yaml file for seperate tests
+
+});*/
