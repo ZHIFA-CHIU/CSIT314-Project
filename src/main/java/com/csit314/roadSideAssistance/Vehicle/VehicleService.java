80503dc561d0af6a6ddcd3a9f57233ac15b834b8
JGripton <102507947+JGripton@users.noreply.github.com>
2022-05-23 20:52:07 +1000

    Add files via upload

--- src/main/java/com/csit314/roadSideAssistance/Vehicle/VehicleService.java
+++ src/main/java/com/csit314/roadSideAssistance/Vehicle/VehicleService.java
@@ -26,8 +26,6 @@ public class VehicleService {
         return vehicleRepository.findAll();
     }
 
-
-
     public void registerVehicle(Vehicle vehicle, Long customerID) {
         Optional<Vehicle> foundVehicle = vehicleRepository.findVehicleByRego(vehicle.getRego());
 
