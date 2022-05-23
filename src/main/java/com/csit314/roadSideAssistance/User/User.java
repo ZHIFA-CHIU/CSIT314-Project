f86b735bbc4f03678e95b83ba0764bc42da889bf
JGripton <102507947+JGripton@users.noreply.github.com>
2022-05-23 13:46:40 +1000

    Added hashPassword to User

--- src/main/java/com/csit314/roadSideAssistance/User/User.java
+++ src/main/java/com/csit314/roadSideAssistance/User/User.java
@@ -1,9 +1,16 @@
 package com.csit314.roadSideAssistance.User;
 
 import com.csit314.roadSideAssistance.Password.Password;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.time.Month;
@@ -11,36 +18,49 @@ import java.time.Period;
 import java.util.UUID;
 
 @MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
 abstract public class User implements Password {
     @Id
-    @GeneratedValue(generator = "uuidGen")
-    @GenericGenerator(
-            name = "uuidGen",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
-    private UUID id;
+    private Long id;
     private String firstName;
     private String lastName;
-    @Column(unique=true)
+    @Column(unique = true)
     private String email;
     private LocalDate dob;
-    @Column(unique=true)
+    @Column(unique = true)
     private String phoneNumber;
     @Transient
     private int age;
     private String streetAddress;
     private String suburb;
     private String postCode;
-    private String State;
+    private String state;
 
-    private String salt;
-    @Column(name="Password")
+    @Column(name = "Password")
     private String password; //SHA-512 encrypted password
+    private byte[] salt;
 
-    public User() {}
 
-    public User(String firstName, String lastName, String email, LocalDate dob, String phoneNumber, String password) {
+
+    public User(String firstName, String lastName, String email, LocalDate dob,
+                String phoneNumber, String password, String streetAddress,
+                String suburb, String postCode, String state) throws NoSuchAlgorithmException {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dob = dob;
+        this.phoneNumber = phoneNumber;
+        this.streetAddress = streetAddress;
+        this.suburb = suburb;
+        this.postCode = postCode;
+        this.state = state;
+        setPassword(password);
+    }
+    public User(String firstName, String lastName, String email, LocalDate dob, String phoneNumber, String password) throws NoSuchAlgorithmException {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -57,90 +77,12 @@ abstract public class User implements Password {
         this.phoneNumber = phoneNumber;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
     public int getAge() {
         if ((dob != null)) {
             return Period.between(dob, LocalDate.now()).getYears();
         }
         return 0; // dob == null
     }
-
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
-    public String getSuburb() {
-        return suburb;
-    }
-
-    public void setSuburb(String suburb) {
-        this.suburb = suburb;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
-    }
-
-    public String getState() {
-        return State;
-    }
-
-    public void setState(String state) {
-        State = state;
-    }
-
-
 
     public boolean validateUser() {
         if (firstName.equals("") || lastName.equals("") || email.equals("") || dob.isBefore(LocalDate.of(1920, Month.JANUARY, 1))) {
@@ -152,29 +94,37 @@ abstract public class User implements Password {
         return true;
     }
 
-    public String hashPassword(String password) {
-        //hashed password to be implemented here
+    public String hashPassword(String password, byte[] salt) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(salt);
+            byte[] bytes = messageDigest.digest(password.getBytes());
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                stringBuilder.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            password = stringBuilder.toString();
+        } catch (NoSuchAlgorithmException error) {
+            error.printStackTrace();
+        }
         return password;
     }
+
     public void generateSalt() {
         //generate random salt
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
-        this.salt = new String(salt);
+        this.salt = salt;
     }
 
     public void setPassword(String password) {
-        if(salt == null) {
-            generateSalt();
-        }
-        this.password = hashPassword(salt+password);
-        assert(this.password != null);
+        generateSalt(); //Changes the salt every time password is changed
+        this.password = hashPassword(password, salt);
     }
 
     public boolean checkPassword(String password) {
-        System.out.println("Input password: " + password + " salt: " + salt + " hashedPassword: " + hashPassword(salt+password) + " stored password: "  + this.password);
-        if(hashPassword(salt+password).equals(this.password)) {
+        if(hashPassword(password, salt).equals(this.password)) {
             return true;
         }
         return false;
