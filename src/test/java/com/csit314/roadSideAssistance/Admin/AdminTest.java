6242c339415265b6a1d384ada9d9c148e8a57264
JGripton <102507947+JGripton@users.noreply.github.com>
2022-05-23 14:05:01 +1000

    Admin tests

---
+++ src/test/java/com/csit314/roadSideAssistance/Admin/AdminTest.java
@@ -0,0 +1,39 @@
+package com.csit314.roadSideAssistance.Admin;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+    Admin adminTest = new Admin("admin@admin.co", "0404567765","password12");
+
+    @Test
+    @DisplayName("Simple Test of admin login")
+    void checkPassword() {
+        assertEquals(adminTest.checkPassword("password12"), true, "Password checker");
+    }
+
+    @Test
+    @DisplayName("Simple Test of admin set password")
+    void setPassword() {
+        adminTest.setPassword("password123");
+        assertEquals(adminTest.checkPassword("password123"), true, "Checking password has been changed");
+    }
+
+    @Test
+    @DisplayName("Simple Test of admin validation")
+    void validateAdmin() {
+        Admin adminTest2 = new Admin("adminadmin.co", "0404567765","password");
+        Admin adminTest3 = new Admin("admin@admin.co", "040456776512345678","password");
+        assertEquals(adminTest.validateAdmin(), true, "Checking admin passes test");
+        assertEquals(adminTest2.validateAdmin(), false, "Checking admin fails test if email wrong");
+        assertEquals(adminTest3.validateAdmin(), false, "Checking admin fails test if mobile wrong");
+    }
+    @Test
+    @DisplayName("Simple Test of admin set password")
+    void ensureHashed() {
+       assertNotEquals("password", adminTest.getPasswordHash(), "Ensuring password is not plaintext");
+    }
+
+}
