33ebfbfbd62179ea609641c3b7b8774041dbac12
Kye <72113800+KyeJones@users.noreply.github.com>
2022-05-23 14:50:45 +1000

    Add files via upload

---
+++ FrontEnd/src/pages/AcceptJob/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../../components/Banner'
+import AcceptJobContent from '../../components/AcceptJobContent/index.jsx'
+
+export default function AcceptJob() {
+    return (
+        <div className='acceptjob-page'>
+            <Banner />
+            <AcceptJobContent />            
+        </div>
+    )
+}
