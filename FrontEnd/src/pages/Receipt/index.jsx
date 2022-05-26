dc9400569c04457d8b63daa50f47b2c240923e43
Zhifa Qiu <zhifaq@zhifas-air.lan.uow.edu.au>
2022-05-26 14:05:53 +1000

    receipt page

--- FrontEnd/src/pages/Receipt/index.jsx
+++ FrontEnd/src/pages/Receipt/index.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
-import { useLocation } from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react'
+import Banner from "../../components/Banner"
+import { getAllJobsRequest } from '../../api';
+import { Table, TableCell, TableContainer, Paper, 
+    TableHead, TableRow, TableBody,  } from '@mui/material';
 /**
  * payer
  * amount
@@ -11,15 +13,69 @@ import { useLocation } from 'react-router-dom';
  */
 export default function Receipt() {
     // getting order
-    let history = useLocation();
-    let [order, _] = useState(history.state.order);
-    console.log(order);
+    // let history = useLocation();
+    // let [order, _] = useState(history.state.order);
+    // console.log(order);
+    const [receiptInfo, setReceiptInfo] = useState({});
+
+    useEffect(
+        () => getAllJobsRequest(2).then(
+            response => {
+                // console.log(response.data[0]);
+                const customer = response.data[0].customer;
+                const technician = response.data[0].technician;
+                console.log(customer);
+                console.log(technician);
+                setReceiptInfo(
+                    {
+                        orderId: response.data[0].id,
+                        customer: `${customer.firstName} ${customer.lastName}`,
+                        technician: `${technician.firstName} ${technician.lastName}`,
+                        price: response.data[0].jobPrice,
+                        category: response.data[0].repairCategory,
+                        additionalInfo: response.data[0].additionalInfo
+                    }
+                );
+                // console.log(receiptInfo);
+            }
+        ).catch(
+            err => alert(err)
+        )
+        , []);
 
     return (
         <div className='Receipt'>
-            <h1>Payer: {order.payer.name.given_name}</h1>
-            <h1>Amount: ${order.purchase_units[0].amount.value}</h1>
-            <h1>Description: {order.purchase_units[0].description}</h1>
+            <Banner />
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Receipt ID</TableCell>
+                            <TableCell align="right">Customer</TableCell>
+                            <TableCell align="right">Technician</TableCell>
+                            <TableCell align="right">Repair Type</TableCell>
+                            <TableCell align="right">Additional Information</TableCell>
+                            <TableCell align="right">Amount</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            <TableRow
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {receiptInfo.orderId}
+                                </TableCell>
+                                <TableCell align="right">{receiptInfo.customer}</TableCell>
+                                <TableCell align="right">{receiptInfo.technician}</TableCell>
+                                <TableCell align="right">{receiptInfo.category}</TableCell>
+                                <TableCell align="right">{receiptInfo.additionalInfo}</TableCell>
+                                <TableCell align="right">${receiptInfo.price}</TableCell>
+                            </TableRow>
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
