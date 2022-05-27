d741cd30e79ba5fa92e6c32b81365ecd77181b48
nwilde <nathan.wilde1994@gmail.com>
2022-05-27 12:40:19 +1000

    Close job page ui uncommented needed code, will probably need tweaking with the states as unsure what the previous pages are passing

--- FrontEnd/src/components/TechnicianRepairInProgressContent/index.jsx
+++ FrontEnd/src/components/TechnicianRepairInProgressContent/index.jsx
@@ -5,23 +5,20 @@ import {useLocation, useNavigate} from 'react-router-dom';
 
 export default function TechnicianRepairInProgressContent() {
     let history = useLocation();
-    //let [ jobId, _ ] = useState(history.state.jobId);
+    let [ jobId, _ ] = useState(history.state.jobId);
 
     const navigate = useNavigate();
 
     const closeJobRequest = (e) => {
-        //api to close job, will uncomment when previous technician repair pages are available
-        //to provide state with jobId
-
-        // closeJob(jobId).then(
-        //     response => {
-        //         navigate("/TechnicianDashboard");
-        //     }
-        // ).catch(
-        //     error => {
-        //         alert(error);
-        //     }
-        // )
+        closeJob(jobId).then(
+            response => {
+                navigate("/TechnicianDashboard");
+            }
+        ).catch(
+            error => {
+                alert(error);
+            }
+        )
         navigate("/TechnicianDashboard");
     };
 
