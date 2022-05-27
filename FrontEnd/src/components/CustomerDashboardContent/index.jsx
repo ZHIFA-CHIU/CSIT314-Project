809c7c516074172b2130af9b981ef9adf887ff2c
shirley <starrysky.su@yahoo.com>
2022-05-27 12:24:11 +1000

    fix bug

--- FrontEnd/src/components/CustomerDashboardContent/index.jsx
+++ FrontEnd/src/components/CustomerDashboardContent/index.jsx
@@ -25,7 +25,7 @@ const CustomerDashboard = () => {
     }
 
     const navServiceHistory = (e) => {
-        navigate('/customerRating');
+        navigate('/customerRating', {state: {"customerId": id}});
     }
 
     const navUpdateDetails = (e) => {
